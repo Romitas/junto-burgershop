a1c60572752af501b8e2355847383a260fe95d39
Romitas <surdeykin.denis@gmail.com>
2017-08-07 11:35:59 +0300

    Add models

---
+++ burgershop/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(default=0.0)
