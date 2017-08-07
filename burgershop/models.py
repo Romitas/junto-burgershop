1df25b1965eeed7754924c385f026d24821a206a
Romitas <surdeykin.denis@gmail.com>
2017-08-07 11:39:25 +0300

    quickfix

--- burgershop/models.py
+++ burgershop/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
