b57dc4a49159f745816e4378a13e831244dfce61
Romitas <surdeykin.denis@gmail.com>
2017-08-07 11:37:58 +0300

    Add burgershop to ISTALLED_APPS

--- junto/settings.py
+++ junto/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'burgershop.apps.BurgershopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
